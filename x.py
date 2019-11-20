d47a227c83b3dcbb560ae36cbb303abd60c59e62
yinxin46 <854864679@qq.com>
2019-11-20 23:13:06 +0800

    GUI代码（合着用）

---
+++ x.py
@@ -0,0 +1,44 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_TabWidget(object):
+
+    def setupUi(self, TabWidget):
+        TabWidget.setObjectName("TabWidget") 
+        TabWidget.resize(789, 619)
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab") 
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setGeometry(QtCore.QRect(10, 30, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.tab)
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 90, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(self.tab)
+        self.label.setGeometry(QtCore.QRect(110, 30, 461, 311))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.tab)
+        self.label_2.setGeometry(QtCore.QRect(620, 30, 151, 341))
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        TabWidget.addTab(self.tab, "")
+        self.tab1 = QtWidgets.QWidget() 
+        self.tab1.setObjectName("tab1")
+        TabWidget.addTab(self.tab1, "")
+        self.tab_2 = QtWidgets.QWidget() 
+        self.tab_2.setObjectName("tab_2")
+        TabWidget.addTab(self.tab_2, "")
+
+        self.retranslateUi(TabWidget)
+        TabWidget.setCurrentIndex(0)
+        self.pushButton.clicked.connect(TabWidget.imageprocessing) 
+        self.pushButton_2.clicked.connect(TabWidget.videoprocessing) 
+        QtCore.QMetaObject.connectSlotsByName(TabWidget)
+
+    def retranslateUi(self, TabWidget):
+        _translate = QtCore.QCoreApplication.translate
+        TabWidget.setWindowTitle(_translate("TabWidget", "TabWidget"))
+        self.pushButton.setText(_translate("TabWidget", "图片"))
+        self.pushButton_2.setText(_translate("TabWidget", "视频"))
+        TabWidget.setTabText(TabWidget.indexOf(self.tab), _translate("TabWidget", "Tab 1"))
+
